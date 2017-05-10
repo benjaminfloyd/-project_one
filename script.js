04cddf03fabee6c5c09ccf29d70b8ea2c49c3d9e
benjaminfloyd <benjamin.floyd@me.com>
2017-05-10 19:30:45 -0400

    4th commit modal class edit

--- script.js
+++ script.js
@@ -2,4 +2,17 @@ $(function(){
     $(document).on("click", "#submit_btn", function(event){
         alert( "GO" );
     });
+
+    // Varying modal content
+    $('#example').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var recipient = button.data('whatever') // Extract info from data-* attributes
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var modal = $(this)
+  modal.find('.modal-title').text('New message to ' + recipient)
+  modal.find('.modal-body input').val(recipient)
+})
+
+
 });
